4c8737168596ecf6a6fcfef57bc7ec4c95727a2e
Michael Marner <michael@20papercups.net>
2020-11-15 14:47:43 +1030

    Remove empty  folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,13 +32,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/content/wp-content`,
         name: `wp-content`,
       },
